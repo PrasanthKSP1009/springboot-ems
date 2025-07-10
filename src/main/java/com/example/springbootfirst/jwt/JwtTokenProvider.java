ce5a960f3786e113f5ef47d3811d522230e3af3e
Prasanth K S <ksprasanthksp10@gmail.com>
2025-07-10 18:42:52 +0530

    With JWT

---
+++ src/main/java/com/example/springbootfirst/jwt/JwtTokenProvider.java
@@ -0,0 +1,55 @@
+package com.example.springbootfirst.jwt;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${app.jwt-secret}")
+    private String jwtSecret;
+
+    @Value("${app.jwt-expiration-milliseconds}")
+    private long jwtExpirationDate;
+
+    private Key getKey() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public String generateToken(Authentication authentication) {
+        String username = authentication.getName();
+        Date now = new Date();
+        Date expiry = new Date(now.getTime() + jwtExpirationDate);
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(now)
+                .setExpiration(expiry)
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String getUsername(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        Jwts.parserBuilder()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(token);
+        return true;
+    }
+}
--- src/main/java/com/example/springbootfirst/repository/RegisterDetailsRepository.java
+++ src/main/java/com/example/springbootfirst/repository/RegisterDetailsRepository.java
@@ -1,5 +1,6 @@
 package com.example.springbootfirst.repository;
 
+import com.example.springbootfirst.jwt.JwtResponseDto;
 import com.example.springbootfirst.models.RegisterDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 
